e01bc2e4856c75307ee0542b0e4b4762d458af8f
aparicioadrii <adrianaparicio20012005@gmail.com>
2024-12-03 12:52:52 +0100

    4.1

--- java4.0/ejemplo1.1.js
+++ java4.0/ejemplo1.1.js
@@ -10,6 +10,16 @@
 // document.write("<tr><td>" + nombre + "</td></tr>")
 // document.write("</table>")
 
+ // Asociar eventos con JavaScript
+
+ // Cambiar texto del div con id mi caja cuando salgo de enciam del elemento
+
+
+ document.getElementById("color_button").onclick = cambiarColor;
+ document.getElementById("color_div").onmouseover = hola;
+ document.getElementById("color_div").onmouseleave = adios;
+
+
 function mostrarFexha() {
     var now = Date();
     console.log("Ahora son las" + now);
@@ -26,9 +36,8 @@ micaja.onmouseover = () => {
 
 
 }
-// Cambiar texto del div con id mi caja cuando salgo de enciam del elemento
+
 micaja.onmouseleave = () => {
     micaja.innerHTML = "Hasta luego!";
 }
 
-    z
